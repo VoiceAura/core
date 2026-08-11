b17e7760d9508643ded56e0cdce2d49a43ccdf80
Emerson Albino <emersonalbino019@gmail.com>
2026-08-11 17:07:27 +0100

    Feat: postgres-schema-cloned-voices
       Add alembic for migrations
       Add connection to postgres
       Add the model of Voice
       Voice: An entity on db which keeps the cloned
       voices data

---
+++ api/db/Session.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+load_dotenv()
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+SessionLocal = sessionmaker(autocomit=False, autoflush=False, bind=engine)
